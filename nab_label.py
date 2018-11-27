2cf0ae3f5755b9ebff5742068101a1a242b1d2fe
Anand191 <anand1911991@outlook.com>
2018-11-27 13:04:31 +0100

    calendar features complete

--- nab_label.py
+++ nab_label.py
@@ -2,24 +2,45 @@ from nab_dataset import add_labels
 import os
 import json
 import pandas as pd
+from numenta_features import calendar_features
 
-data_files = os.listdir('./resources/Numenta/data/realAdExchange')
-file_paths = [os.path.join('./resources/Numenta/data/realAdExchange',f) for f in data_files]
-label_path = './resources/Numenta/labels/combined_windows.json'
-with open(label_path) as f:
-    json_labels = json.load(f)
+# data_files = os.listdir('./resources/Numenta/data/realAdExchange')
+# file_paths = [os.path.join('./resources/Numenta/data/realAdExchange',f) for f in data_files]
+# label_path = './resources/Numenta/labels/combined_windows.json'
+# with open(label_path) as f:
+#     json_labels = json.load(f)
+#
+#
+#
+# columns = ['timestamp', 'value', 'anomaly']
+# destination = './resources/Numenta/cleaned_data/window'
+# if not os.path.exists(destination):
+#     os.makedirs(destination)
+# process_data = add_labels(file_paths, label_path, 'csv', window=True)
+# all_dataframes = process_data.process()
+# for i, df in enumerate(all_dataframes):
+#     # print(data_files[i])
+#     # print(json_labels['realAdExchange'+'/'+data_files[i]])
+#     # print(df.loc[df['anomaly'] == 1])
+#     dirname = os.path.dirname(file_paths[i]).split('/')[-1]
+#     df.to_csv(os.path.join(destination, "{}-{}".format(dirname,data_files[i])), sep=',', index_label=None, columns=columns)
 
+path = 'resources/Numenta/cleaned_data/window/realAdExchange-exchange-2_cpc_results.csv'
+add_calender_features = calendar_features(path)
+print(add_calender_features.df.columns)
 
+add_calender_features.add_weekdays()
+print(add_calender_features.df.columns)
+add_calender_features.reset()
 
-columns = ['timestamp', 'value', 'anomaly']
-destination = './resources/Numenta/cleaned_data/window'
-if not os.path.exists(destination):
-    os.makedirs(destination)
-process_data = add_labels(file_paths, label_path, 'csv', window=True)
-all_dataframes = process_data.process()
-for i, df in enumerate(all_dataframes):
-    # print(data_files[i])
-    # print(json_labels['realAdExchange'+'/'+data_files[i]])
-    # print(df.loc[df['anomaly'] == 1])
-    dirname = os.path.dirname(file_paths[i]).split('/')[-1]
-    df.to_csv(os.path.join(destination, "{}-{}".format(dirname,data_files[i])), sep=',', index_label=None, columns=columns)
+add_calender_features.add_week()
+print(add_calender_features.df.columns)
+add_calender_features.reset()
+
+add_calender_features.add_month()
+print(add_calender_features.df.columns)
+add_calender_features.reset()
+
+add_calender_features.add_quarter()
+print(add_calender_features.df.columns)
+add_calender_features.reset()
--- numenta_features.py
+++ numenta_features.py
@@ -1,5 +1,54 @@
 import pandas as pd
+import numpy as np
+from collections import Counter
 
-df = pd.read_csv('resources/Numenta/cleaned_data/window/realAdExchange-exchange-2_cpc_results.csv', sep=',', index_col=0)
-df['timestamp'] = df['timestamp'].apply(lambda x: pd.to_datetime(x))
-print(df['timestamp'].iloc[1].strftime('%A'))
+
+class calendar_features(object):
+    '''
+
+    '''
+
+    ALL_DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    ALL_WEEKS = list(range(1,53))
+    ALL_MONTHS = list(range(1,13))
+    ALL_QUARTERS = list(range(1,5))
+    KEYS = {'days':ALL_DAYS, 'weeks':ALL_WEEKS, 'months':ALL_MONTHS, 'quarters':ALL_QUARTERS}
+
+    def __init__(self, path, dummy=True):
+        self.dummy = dummy
+        self.path = path
+        self.reset()
+
+    def _makeCategorical(self, col_name, prefix):
+        self.df = pd.get_dummies(self.df, prefix=[prefix], columns=[col_name], drop_first=self.dummy)
+
+    def _dummyVsOnehot(self,col_name, key):
+        if not Counter(np.unique(self.df[col_name].values).tolist()) == Counter(calendar_features.KEYS[key]):
+            self.dummy = False
+
+    def add_weekdays(self):
+        self.df['timestamp'] = self.df['timestamp'].apply(lambda x: pd.to_datetime(x))
+        self.df['day_of_week'] = self.df['timestamp'].dt.day_name()
+        self._dummyVsOnehot('day_of_week', 'days')
+        self._makeCategorical('day_of_week', 'day')
+
+    def add_week(self):
+        self.df['timestamp'] = self.df['timestamp'].apply(lambda x: pd.to_datetime(x))
+        self.df['week_of_year'] = self.df['timestamp'].dt.week
+        self._dummyVsOnehot('week_of_year', 'weeks')
+        self._makeCategorical('week_of_year', 'week')
+
+    def add_month(self):
+        self.df['timestamp'] = self.df['timestamp'].apply(lambda x: pd.to_datetime(x))
+        self.df['month_of_year'] = self.df['timestamp'].dt.month
+        self._dummyVsOnehot('month_of_year', 'months')
+        self._makeCategorical('month_of_year', 'month')
+
+    def add_quarter(self):
+        self.df['timestamp'] = self.df['timestamp'].apply(lambda x: pd.to_datetime(x))
+        self.df['quarter_of_year'] = self.df['timestamp'].dt.quarter
+        self._dummyVsOnehot('quarter_of_year', 'quarters')
+        self._makeCategorical('quarter_of_year', 'quarter')
+
+    def reset(self):
+        self.df = pd.read_csv(self.path, sep=',', index_col=0)
